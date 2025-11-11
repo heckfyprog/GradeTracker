14c2bca4f930f50124571b76a6ae399e5bb39fc9
Ruslan Mukhin <mukhinruslan43@gmail.com>
2025-11-11 13:06:05 +0100

    Add project files.

---
+++ GradeTracker/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace GradeTracker
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ GradeTracker/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace GradeTracker
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
