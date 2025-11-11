d771819d2156de45804291a3ec5f462403ccf519
Ruslan Mukhin <mukhinruslan43@gmail.com>
2025-11-11 15:10:40 +0100

    Created a framework for folders and files for the MVVM architecture.

---
+++ GradeTracker/Models/Enums/ClaasType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GradeTracker.Models.Enums
+{
+    public enum ClassType
+    {
+        Lecture,
+        Tutorial,
+        Laboratory
+    }
+}
---
+++ GradeTracker/Models/University/YearOfStudy.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GradeTracker.Models.University
+{
+    internal class YearOfStudy
+    {
+    }
+}
